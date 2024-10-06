091c4d8c460c1e7d99e17a61146f57aab360dd5e
Bindu Varsha <binduvarsha2004@gmail.com>
2024-10-06 11:15:46 +0530

    intial commit

---
+++ Next.js-Creative-Portfolio-Website-main/src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Nexusmart",
+    description: "An Integrated College Management System",
+    date: "2024-08-15",
+    demoLink: "https://nexusmart.example.com",
+  },
+  {
+    id: 2,
+    name: "Mail Easy",
+    description: "Automatic Email Sending System",
+    date: "2023-06-20",
+    demoLink: "https://maileasy.example.com",
+  },
+  {
+    id: 3,
+    name: "Dogy",
+    description: "Dog selling website",
+    date: "2022-09-10",
+    demoLink: "https://dogy.example.com",
+  },
+  {
+    id: 4,
+    name: "Meeshi",
+    description: "Meesho website clone",
+    date: "2024-05-30",
+    demoLink: "https://meeshi.example.com",
+  },
+  {
+    id: 5,
+    name: "Spotifee",
+    description: "Spotify website clone",
+    date: "2024-07-12",
+    demoLink: "https://spotifee.example.com",
+  },
+  {
+    id: 6,
+    name: "SmallBasket",
+    description: "An E-commerce grocery site",
+    date: "2024-10-01",
+    demoLink: "https://smallbasket.example.com",
+  },
+  {
+    id: 7,
+    name: "Jarvis",
+    description: "An AI agent",
+    date: "2024-04-18",
+    demoLink: "https://jarvis.example.com",
+  },
+  {
+    id: 8,
+    name: "HexaMatch",
+    description: "Tile matching game",
+    date: "2023-11-05",
+    demoLink: "https://hexamatch.example.com",
+  },
+  {
+    id: 9,
+    name: "Portfolio",
+    description: "Stunning Portfolio Site",
+    date: "2023-03-22",
+    demoLink: "https://portfolio.example.com",
+  },
+  {
+    id: 10,
+    name: "Tuner",
+    description: "Virtual musical instruments playing web app",
+    date: "2022-02-14",
+    demoLink: "https://tuner.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/bindu-varsha-yaddanapudi-80a7b0243/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com/",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ Next.js-Creative-Portfolio-Website-main/src/components/about/index.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+          Pioneer of Exploration
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+          I am a diligent & dedicated individual with a passion for
+          continuous learning & active involvement in the tech community.
+          I leverage rapid adaptation, keen curiosity, and critical problem solving skills to drive innovation. With a penchant for
+          experimentation and an optimistic mindset, I effectively navigate
+          challenges and contribute meaningfully to cutting-edge projects.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            200+<sub className="font-semibold text-base">LeetCode Q's</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            4{" "}
+            <sub className="font-semibold text-base">Programming Languages</sub>
+          </p>
+        </ItemLayout>
+
+        {/* <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=python,java,c,cpp,html,css,figma,bootstrap,git,github,mysql`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        {/* <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="CodeBucks"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout> */}
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
